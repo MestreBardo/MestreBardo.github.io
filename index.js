c601402863456bf80abb30d6dcf2c202f93ed594
vinycius monteiro <vinyciussilvamonteiro@gmail.com>
2020-05-07 22:40:55 -0300

    Novo site naruto

--- index.js
+++
@@ -1,8 +0,0 @@
-if(localStorage.getItem('usuario') === null){
-    let math = '';
-    for (let index = 0; index < 3; index++) {
-        math += Math.floor(Math.random() * 100); 
-        
-    }
-    localStorage.setItem('usuario',math);
-}
